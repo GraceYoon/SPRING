a172ecbec5ad88c2ed65c75225c000ae6ce6dd17
Grace Yoon <gyoon@stat.tamu.edu>
2019-12-10 14:05:54 -0600

    edit comments

--- man/examples/synthData_ex.R
+++ man/examples/synthData_ex.R
@@ -2,12 +2,12 @@
 
 require(SpiecEasi)
 
-# goal is to generate synthetic data with prescribed graph structure.
+# goal is to generate synthetic data with a prescribed graph structure.
 # load real data "QMP" in SPRING package.
 data(QMP)
 set.seed(12345) # set the seed number for make_graph part.
-p1 = ncol(QMP)
-e1 = 2*p1 # number of edges are twice of the number of nodes/variables.
+p1 = ncol(QMP) # the number of nodes.
+e1 = 2*p1 # the number of edges is set as twice the number of nodes.
 gtype = "cluster"
 # available types in SpiecEasi: "band", "cluster", "scale_free", "erdos_renyi", "hub", "block".
 graph_p1 <- SpiecEasi::make_graph(gtype, p1, e1) # adjacency matrix. 1: edge, 0: no edge.
@@ -16,7 +16,7 @@ Cor1   <- cov2cor(SpiecEasi::prec2cov(Prec1)) # correlation matrix.
 
 X1_count <- synthData_from_ecdf(QMP, Sigma = Cor1, n = 100)
 # generate data of size n by p.
-# p = ncol(Cor1) = ncol (QMP) should hold.
+# p = ncol(Cor1) = ncol(QMP) should hold.
 # need to specify sample size n.
 
 
